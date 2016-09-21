d1c99472bc898ff482d33572a59093aba1ffbc95
lucans <luuucans@gmail.com>
2016-09-21 08:05:56 -0300

    Lgin

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,7 @@
         .state('login', {
           url: "/Login",
           templateUrl: "partials/login.html",
-          controller: "menuCtrl"
+          controller: "userCtrl"
         })
 }) 
 
@@ -51,10 +51,6 @@
 
 app.controller("homeCtrl", ['$scope', '$http', '$rootScope','DateProvider', function ($s, $http, $rs, Date) {
 
-    $s.viewTarefa = function(tarefa){
-        $s.tarefaselecionada = tarefa;
-    }
-
 }]);
 
 app.controller("menuCtrl", ['$scope', '$http', '$rootScope','$location', function ($s, $http, $rs, $location) {            
@@ -72,27 +68,6 @@ app.controller("menuCtrl", ['$scope', '$http', '$rootScope','$location', functio
             $location.path(rota);
         }
     };
-
-    $s.verificaUserSession = function(){
-        $s.p = 'verifyUserSession';
-        $http.get("server/dao/redirect.php?p="+$s.p).success(function(result) {
-            if (result != 'false') {                    
-                $rs.user = result[0];
-            } else {
-                $s.showToast("Sessão expirada!");
-                $s.goRota('/Login');                
-            }    
-        });
-    };
-
-    $s.userAuth = function(oUser){            
-        $s.p = 'userAuth';
-        $http.post("server/dao/redirect.php?p=" + $s.p, {
-            oUser: oUser            
-        });
-        $s.goRota('Home');        
-    };         
-
 
     $s.getTotal = function(oDados){          
         $s.p = 'getTotal';
@@ -187,9 +162,57 @@ app.controller("menuCtrl", ['$scope', '$http', '$rootScope','$location', functio
         $http.post("server/dao/redirect.php?p=" + $s.p, codtarefa).success($s.getTarefas());
     }
 
+
+
     $s.getCustos();
     $s.getTarefas();
     $s.getHoras();
+
+}]);
+
+app.controller("userCtrl", ['$scope', '$http', '$rootScope','$location', function ($s, $http, $rs, $location) {            
+        
+    $s.teste = function(){
+        console.log("working");
+    }
+        
+    $s.showToast = function(message){
+        Materialize.toast(message, 3000);
+    };
+
+   $s.goRota = function(rota){ 
+        if (rota) {
+            $location.path(rota);
+        }
+    };
+
+    $s.userAuth = function(oUser){          
+        $s.p = 'userAuth';
+        $http.post("server/dao/redirect.php?p=" + $s.p, {
+            oUser: oUser
+        }).success(function(result){
+            $rs.user = result;
+            if ($rs.user) { 
+                $s.goRota('/Home');
+            } else {
+                $s.showToast('Usuário ou senha inválidos.');
+            }
+        });
+    }
+
+    // $s.verificaUserSession = function(){
+        if (!$rs.user) { 
+            $s.goRota('/Login') 
+        }
+    // };
+
+    // $s.userAuth = function(oUser){            
+    //     $s.p = 'userAuth';
+    //     $http.post("server/dao/redirect.php?p=" + $s.p, {
+    //         oUser: oUser            
+    //     });
+    //     $s.goRota('Home');        
+    // };         
 
 }]);
 
--- server/dao/conexao.php
+++ server/dao/conexao.php
@@ -1,12 +1,12 @@
 <?
-// if ($_SERVER['DOCUMENT_ROOT'] == 'C:/Users/Lucas/Documents/GitHub') {
-// 	mysql_connect('192.168.10.20','root','proxy');
-// } else{
-// 	mysql_connect('localhost','root','');
-// }
+if ($_SERVER['DOCUMENT_ROOT'] == 'C:/Users/Lucas/Documents/GitHub') {
+	mysql_connect('192.168.10.20','root','proxy');
+} else{
+	mysql_connect('localhost','root','');
+}
 
 
-mysql_connect('www.lucans.com.br','lucas','meupenis');
+// mysql_connect('www.lucans.com.br','lucas','meupenis');
 
 mysql_select_db("db_lucas");
 
